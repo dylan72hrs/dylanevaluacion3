48ae0357cdaac5421b51c30462b2bf55fc6f3f76
dylan72hrs <53924811+dylan72hrs@users.noreply.github.com>
2024-05-31 19:51:29 -0400

    sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,24 +1,51 @@
-#!/bin/bash
+import hashlib
+import sqlite3
+from flask import Flask, request
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+app = Flask(__name__)
 
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
+db_name = 'users.db'
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+@app.route('/')
+def index():
+    return 'Bienvenido al sistema de gestión de claves'
 
-cd tempdir
+@app.route('/signup', methods=['POST'])
+def signup():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS USERS
+                 (USERNAME TEXT PRIMARY KEY NOT NULL,
+                  HASH TEXT NOT NULL);''')
+    conn.commit()
+    username = request.form['username']
+    password = request.form['password']
+    hash_value = hashlib.sha256(password.encode()).hexdigest()
+    try:
+        c.execute("INSERT INTO USERS (USERNAME, HASH) VALUES (?, ?)", (username, hash_value))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        return "El usuario ya está registrado"
+    return "Registro exitoso"
 
-docker build -t sampleapp .
+def verify(username, password):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    query = "SELECT HASH FROM USERS WHERE USERNAME = ?"
+    c.execute(query, (username,))
+    records = c.fetchone()
+    conn.close()
+    if not records:
+        return False
+    return records[0] == hashlib.sha256(password.encode()).hexdigest()
 
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if verify(username, password):
+        return "Inicio de sesión exitoso"
+    return "Nombre de usuario o contraseña incorrectos"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6000)
